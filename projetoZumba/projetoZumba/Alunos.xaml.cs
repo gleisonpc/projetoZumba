4734d71d08a8e1b1d575eadca85896d3a70a65df
gabrielinaciodemello <gabrielinaciodemello@gmail.com>
2014-02-13 11:57:00 -0200

    Impementada a funcionalidade visualizar alunos

---
+++ projetoZumba/projetoZumba/Alunos.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace projetoZumba
+{
+    /// <summary>
+    /// Interaction logic for Alunos.xaml
+    /// </summary>
+    public partial class Alunos : Window
+    {
+        AlunosModel alunosModel = new AlunosModel();
+
+        public Alunos()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            alunosModel.mostrarAlunos(DataGridAlunos);
+        }
+    }
+}
---
+++ projetoZumba/projetoZumba/AlunosModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetoZumba
+{
+    class AlunosModel
+    {
+        internal void mostrarAlunos(System.Windows.Controls.DataGrid DataGridAlunos)
+        {
+            gerjfdEntities context = new gerjfdEntities();
+            DataGridAlunos.ItemsSource = context.gerjfd_aluno.ToList();
+        }
+    }
+}
