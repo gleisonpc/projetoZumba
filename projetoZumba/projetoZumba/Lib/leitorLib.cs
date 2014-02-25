9fbbaf873b794580c3075ee5058b8e8a85a153ef
gleisonpc <gleisonpc@gmail.com>
2014-02-25 20:36:21 -0300

    Teste

--- projetoZumba/projetoZumba/Lib/leitorLib.cs
+++ projetoZumba/projetoZumba/Lib/leitorLib.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Drawing;
 using System.Windows;
 
+//CLASSE RESPONSABEL POR CONTROLAR A LEITURA DE DIGITAL
 namespace projetoZumba.Lib
 {
     class leitorLib : DPFP.Capture.EventHandler
