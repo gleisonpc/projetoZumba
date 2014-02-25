8f9f5963e6e46806989c9604392ec5a45d8c1ec5
gabrielinaciodemello <gabrielinaciodemello@gmail.com>
2014-02-25 19:54:24 -0300

    Gravação de digital no banco concluida

--- projetoZumba/projetoZumba/Moldel/AlunosModel.cs
+++ projetoZumba/projetoZumba/Moldel/AlunosModel.cs
@@ -11,7 +11,7 @@ namespace projetoZumba
         internal void mostrarAlunos(System.Windows.Controls.DataGrid DataGridAlunos)
         {
             gerjfdEntities context = new gerjfdEntities();
-            var data = (from p in context.gerjfd_aluno select new { p.aluno_id, p.aluno_nome });
+            var data = (from p in context.gerjfd_aluno select new { p.aluno_id, p.aluno_nome});
             DataGridAlunos.ItemsSource = data.ToList();
         }
     }
