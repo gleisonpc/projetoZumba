97c073c6e3395d6ace01f899499e07a3c9014afe
gleisonpc <gleisonpc@gmail.com>
2014-03-07 10:26:10 -0300

    Implementado funcionalidade Excluir da Modalidade

--- projetoZumba/projetoZumba/Views/Modalidade/EditarModalidade.xaml.cs
+++ projetoZumba/projetoZumba/Views/Modalidade/EditarModalidade.xaml.cs
@@ -29,7 +29,7 @@ namespace projetoZumba.Views
             this.modalidadeBanco = modalidadeBanco;
             this.modalidades = modalidades;
 
-            string t = modalidadeBanco.modalidade_vlrp.ToString();
+            nomeModalidade.Text = modalidadeBanco.modalidade_nome;
 
             if (!modalidadeBanco.modalidade_vlrp.ToString().Contains(","))
             {
@@ -37,14 +37,38 @@ namespace projetoZumba.Views
             }
             else
                 {
-                    //string str[] = modalidadeBanco.modalidade_vlrp.ToString().Split(".");
+                    string[] str = modalidadeBanco.modalidade_vlrp.ToString().Split(',');
+
+                    if (str[1].Length == 1)
+                    {
+                        vlrPrincipal.Text = modalidadeBanco.modalidade_vlrp.ToString() + "0";
+                    }
+                    else
+                    {
+                        vlrPrincipal.Text = modalidadeBanco.modalidade_vlrp.ToString();
+                    }
 
                 }
-            
-           
-            nomeModalidade.Text = modalidadeBanco.modalidade_nome;
-            
-            vlrAdicional.Text = modalidadeBanco.modalidade_vlra.ToString();
+
+            if (!modalidadeBanco.modalidade_vlra.ToString().Contains(","))
+            {
+                vlrAdicional.Text = modalidadeBanco.modalidade_vlra.ToString() + ".00";
+            }
+            else
+            {
+                string[] str = modalidadeBanco.modalidade_vlra.ToString().Split(',');
+
+                if (str[1].Length == 1)
+                {
+                    vlrAdicional.Text = modalidadeBanco.modalidade_vlra.ToString() + "0";
+                }
+                else
+                {
+                    vlrAdicional.Text = modalidadeBanco.modalidade_vlra.ToString();
+                }
+
+            }
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -72,6 +96,16 @@ namespace projetoZumba.Views
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
+            this.Close();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            gerjfdEntities context = new gerjfdEntities();
+            var row = context.gerjfd_modalidade.Find(modalidadeBanco.modalidade_id);
+            context.gerjfd_modalidade.Remove(row);
+            context.SaveChanges();
+            modalidades.updateModalidades();
             this.Close();
         }
     }
